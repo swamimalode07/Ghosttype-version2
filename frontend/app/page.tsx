a476df81ed5daad60379f8ade81167325598a52b
Swami Malode <swamimalode2004@gmail.com>
2026-01-01 00:28:36 +0530

    landing page done

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,100 +1,43 @@
 "use client"
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
+
 export default function Home() {
   const router=useRouter()
-
-  const [username,setUsername]=useState("")
-
-  const [joinUsername,setJoinUsername]=useState("")
-  const [roomCode,setRoomCode]=useState("")
-
-  async function handlecreateRoom(){
-    if(!username){
-      alert("Please enter username")
-      return
-    }
-
-    localStorage.setItem("username",username)
-
-    const response=await fetch("http://127.0.0.1:8000/create-room",{
-      method:"POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body:JSON.stringify({
-        username:username
-      })
-    })
-
-    const data=await response.json()
-    console.log(data)
-    const roomCode=data.room_code
-    router.push(`/room/${roomCode}/lobby`)
-  }
-  
-  async function handleJoinRoom(){
-    if(!joinUsername){
-      alert("Please enter username")
-      return
-    }
-    if(!roomCode){
-      alert("Please enter room code to proceed")
-      return
-    }
-    localStorage.setItem("username",joinUsername)
-
-    const response=await fetch("http://127.0.0.1:8000/join-room",{
-      method:"POST",
-      headers:{
-        "Content-Type": "application/json",
-      },
-      body:JSON.stringify({
-        username: joinUsername,
-        room_code: roomCode,
-      })
-    })
-
-    const data=await response.json()
-    console.log("Join event data",data) 
-    router.push(`/room/${roomCode}/lobby`)
-
-  }
-  
-
   return (
-    <div className="min-h-screen bg-background text-foreground">
-      <div className="grid grid-cols-2 text-center ">
-        <div className="">
-          <div className="min-h-screen flex items-center justify-center">
-            <div className="max-w-md w-full flex flex-col items-center gap-4 border-2 border-grey-500 p-16 rounded-2xl">
-              <h1>Create room</h1>
-              <Input placeholder="Enter username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
-              <Button className="w-full" onClick={handlecreateRoom}>
-                Create Room
-              </Button>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="">
-              <div className="min-h-screen flex items-center justify-center">
-            <div className="max-w-md w-full flex flex-col items-center gap-4 border-2 border-grey-500 p-16 rounded-2xl">
-              <h1>Join room</h1>
-              <Input placeholder="Username" value={joinUsername} onChange={(e)=>setJoinUsername(e.target.value)}/>
-              <Input placeholder="Enter room code" value={roomCode} onChange={(e)=>setRoomCode(e.target.value)}/>
-              <Button className="w-full" onClick={handleJoinRoom}>
-                Join Room
-              </Button>
-            </div>
-          </div>
-          </div>
+   <>
+    <div className="bg-background text-foreground max-w-6xl mx-auto border-r border-l border-gray-500 min-h-screen">
+  <div className="flex justify-between px-10 pt-4 border-b border-gray-500 pb-4">
+    <div className="text-xl font-semibold">Ghosttype</div>
+    <div>button</div>
+  </div>
+
+  <div className="grid grid-cols-8 min-h-[calc(100vh-72px)]">
+    <div className="col-span-5 flex items-center">
+      <div className="flex flex-col gap-6 px-10">
+        <h1 className="text-5xl font-semibold leading-tight">
+          Type faster with <br /> Ghosttype
+        </h1> 
+
+        <div className="flex gap-3">
+          <Button variant="redbutton" onClick={()=>router.push("/choose-option")}>
+            Get started
+          </Button>
+          <button className="rounded-lg bg-[#27272A]  px-4 py-2 text-sm">
+            Open source
+          </button>
         </div>
       </div>
-   </div>
+    </div>
+
+    <div className="col-span-3 flex items-center justify-center">
+      Keyboard
+    </div>
+  </div>
+</div>
+   </>
   );
 }
--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        redbutton:"bg-gradient-to-b from-red-700 to-red-500 border-t-2 border-red-500 text-white hover:bg-red-700 focus-visible:ring-red-300"
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
